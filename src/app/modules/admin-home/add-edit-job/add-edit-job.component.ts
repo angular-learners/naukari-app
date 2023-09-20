9c75e19b5b9234f99cedf4b9d39d852e8957e3eb
codewthjava <abhisunnii@gmail.com>
2023-09-20 10:24:36 +0530

    job add changes

--- src/app/modules/admin-home/add-edit-job/add-edit-job.component.ts
+++ src/app/modules/admin-home/add-edit-job/add-edit-job.component.ts
@@ -1,6 +1,6 @@
 import { SharedService } from './../../../services/shared.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { NaukariConstants } from 'src/app/constants/naukari.constants';
 import { JobsService } from 'src/app/services/jobs.service';
 
@@ -12,7 +12,7 @@ import { JobsService } from 'src/app/services/jobs.service';
 export class AddEditJobComponent implements OnInit {
   addEditJobForm!: FormGroup;
 
-  constructor(private fb: FormBuilder,private jobService:JobsService,public sharedService:SharedService) {
+  constructor(private fb: FormBuilder, private jobService: JobsService, public sharedService: SharedService) {
 
   }
   ngOnInit(): void {
@@ -20,61 +20,67 @@ export class AddEditJobComponent implements OnInit {
     this.sharedService.message;
   }
 
+  form() {
+    this.addEditJobForm = new FormGroup({
+      jobTitle: new FormControl('')
+    })
+  }
+
   onInitAddEditJobsForm() {
     this.addEditJobForm = this.fb.group({
-      "jobTitle": [''],
-      "experience": [''],
-      "package": [''],
-      "companyName": [''],
-      "companyLogo": [''],
-      "description": [''],
-      "locations": this.fb.array([
-         this.fb.group({
-          "locationName":['']
+      jobTitle: [''],
+      experience: [''],
+      package: [''],
+      companyName: [''],
+      companyLogo: [''],
+      description: [''],
+      locations: this.fb.array([
+        this.fb.group({
+          locationName: ['']
         })
       ]),
-      "isActive": [true],
-      "createdDate": [''],
-      "updatedDate": [''],
-      "jobDescription": ['']
+      isActive: [true],
+      createdDate: [''],
+      updatedDate: [''],
+      jobDescription: ['']
     })
   }
 
 
-   get getLocations():FormArray{
+  get getLocations(): FormArray {
     return this.addEditJobForm.get('locations') as FormArray;
-   }
+  }
 
-   addNewLocation(){
+  addNewLocation() {
     return this.fb.group({
-      "locationName":['']
+      locationName: ['']
     })
-   }
+  }
 
-   addLocation(){
-       this.getLocations.push(this.addNewLocation());
-   }
+  addLocation() {
+    this.getLocations.push(this.addNewLocation());
+  }
 
-   removeLocation(index:number){
+  removeLocation(index: number) {
     this.getLocations.removeAt(index);
-   }
+  }
 
 
 
-   saveJob(){
+  saveJob() {
     this.addEditJobForm.get('createdDate')?.setValue(new Date());
-     this.jobService.createJob(this.addEditJobForm.value).subscribe(
+    this.jobService.createJob(this.addEditJobForm.value).subscribe(
       {
-        next:(res:any)=>{
-         console.log(res);
-         this.sharedService.message=NaukariConstants.JOB_CREATED;
-         this.addEditJobForm.reset();
+        next: (res: any) => {
+          console.log(res);
+          this.sharedService.message = NaukariConstants.JOB_CREATED;
+          this.addEditJobForm.reset();
         },
-        error:(err:any)=>{
+        error: (err: any) => {
           console.log(err);
         }
       }
-     )
+    )
     // console.log(this.addEditJobForm.value);
-   }
+  }
 }
